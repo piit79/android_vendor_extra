f490e2252e4c963996aa4742b3e0cbf34245a172
piit79 <piit@zz9.cz>
2024-01-26 23:24:01 +0100

    Change GApps integration logic to opt-in
    Change-Id: I52de88abfde9aaeefb8aa83ccef6263f4cfefa2f

--- product.mk
+++ product.mk
@@ -1,5 +1,5 @@
 # Integrate MindTheGapps into the build
-ifneq ($(INCLUDE_GAPPS),false)
+ifeq ($(INCLUDE_GAPPS),true)
     ifeq ($(TARGET_ARCH),arm64)
         $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
     else
